9d765850acac94ffb3d5aece8bfe4668d19ca60c
App Academy Student <student@appacademy.io>
2014-01-09 19:50:13 -0500

    HumanPlayer class is cleaner

--- lib/humanplayer.rb
+++ lib/humanplayer.rb
@@ -6,21 +6,28 @@ class HumanPlayer
   end
 
   def move_input
-    puts "#{@name}, what move would you like to make?"
-    puts "Example: A3, B4"
-    user_input = gets.chomp
-    format_move(user_input)
+    positions = []
+
+    puts "#{@name}, what piece would you like to move?"
+    puts "Example: A3"
+    user_input = gets.chomp.split
+
+    positions << format_move(user_input)
+
+    puts "Where would you like to move it?"
+    user_input = gets.chomp.split(",")
+    positions << format_move(user_input)
   end
 
   private
   def format_move(user_input)
+    moves = []
     horizontal_directional = ("A".."H").to_a
 
-    start_move, end_move = user_input.split(", ")
+    user_input.each do |move|
+      moves << [(8 - move[1].to_i), horizontal_directional.index(move[0].upcase)]
+    end
 
-    start_move = [(8 - start_move[1].to_i), horizontal_directional.index(start_move[0].upcase)]
-    end_move = [(8 - end_move[1].to_i), horizontal_directional.index(end_move[0].upcase)]
-
-    [start_move, end_move]
+    moves
   end
 end
